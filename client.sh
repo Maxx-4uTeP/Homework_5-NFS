71705e57a68f1e30f19684a7a13e6da0d2187671
Maxx-4uTeP <majorovmv@gmail.com>
2021-05-16 10:17:01 +0300

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,6 @@
+yum install nfs-utils -y
+systemctl start rpcbind
+systemctl enable rpcbind
+mkdir /mnt/nfs-share
+do echo "10.0.0.41:/var/nfs/ /mnt/nfs-share/ nfs defaults 0 0" >> /etc/fstab; done
+do echo "10.0.0.41:/var/upload/ /mnt/nfs-share/upload/ nfs defaults 0 0" >> /etc/fstab; done
