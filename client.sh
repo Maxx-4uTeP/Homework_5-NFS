e986f1f5f4a4618dc1747dd2333d3b495e2a03e2
Maxx-4uTeP <majorovmv@gmail.com>
2021-05-16 11:11:07 +0300

    11

--- client.sh
+++ client.sh
@@ -3,5 +3,9 @@ yum install nfs-utils -y
 systemctl start rpcbind
 systemctl enable rpcbind
 mkdir /mnt/nfs-share
-do echo "10.0.0.41:/var/nfs/ /mnt/nfs-share/ nfs defaults 0 0" >> /etc/fstab; done
-do echo "10.0.0.41:/var/upload/ /mnt/nfs-share/upload/ nfs defaults 0 0" >> /etc/fstab; done
+mkdir /mnt/nfs-share/upload
+mount -t nfs 10.0.0.41:/var/nfs/ /mnt/nfs-share/
+mount -t nfs 10.0.0.41:/var/upload/ /mnt/nfs-share/upload/
+echo "10.0.0.41:/var/nfs/ /mnt/nfs-share/ nfs defaults 0 0" >> /etc/fstab
+echo "10.0.0.41:/var/upload/ /mnt/nfs-share/upload/ nfs defaults 0 0" >> /etc/fstab
+reboot
--- server.sh
+++ server.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-yum install nfs-utils
+yum install nfs-utils -y
 systemctl enable rpcbind nfs-server
 systemctl start rpcbind nfs-server
 mkdir /var/nfs
+mkdir /var/nfs/upload
 mkdir /var/upload
 chmod -R 777 /var/nfs
 chmod -R 777 /var/upload
-do echo "/var/nfs 10.0.0.0/24(ro,root_squash)" >> /etc/exports; done
-do echo "/var/upload 10.0.0.0/24(rw,root_squash)" >> /etc/exports; done
+echo "/var/nfs 10.0.0.0/24(ro,root_squash)" >> /etc/exports
+echo "/var/upload 10.0.0.0/24(rw,root_squash)" >> /etc/exports
 exportfs -r
 systemctl start firewalld
 sudo firewall-cmd --zone=public --add-service=nfs --permanent
 sudo firewall-cmd --zone=public --add-service=nfs3 --permanent
 sudo firewall-cmd --reload
-
+reboot
